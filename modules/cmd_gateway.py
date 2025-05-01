98b8dfe7c7717281b123cfdc8724e9458638cdb6
Ryan Hou <rhou7873@gmail.com>
2025-05-01 19:50:08 -0400

    join/leave vc functionality implemented

--- modules/cmd_gateway.py
+++ modules/cmd_gateway.py
@@ -1,4 +1,5 @@
 import os
+import modules.database as db
 
 import discord
 from discord.ext.commands import Cog
@@ -10,19 +11,25 @@ class GatewayCog(Cog, name="Command Gateway"):
 
     def __init__(self, bot: discord.Bot):
         self.bot = bot
+        self.CLIPPED_SESSIONS_COLLECTION = os.getenv(
+            "CLIPPED_SESSIONS_COLLECTION")
+
+        if self.CLIPPED_SESSIONS_COLLECTION is None:
+            raise Exception(
+                "Clipped sessions collection name is not in environment variables")
 
     ####### COMMAND REGISTRATIONS #######
 
     @commands.slash_command(
-            name="joinvc",
-            description="Prompt Clipped bot to join your voice channel",
-            guild_ids=[os.getenv("DEV_GUILD_ID")])
+        name="joinvc",
+        description="Prompt Clipped bot to join your voice channel",
+        guild_ids=[os.getenv("DEV_GUILD_ID")])
     async def cmd_join_vc(self, ctx: discord.ApplicationContext):
         """ 
         Prompts Clipped bot to join voice channel so it can start listening. This
         command must be used before a clip can be requested.
         """
-        voice = self.join_vc(ctx)
+        voice = await self.join_vc(ctx)
         if voice is None:
             return
 
@@ -30,9 +37,9 @@ class GatewayCog(Cog, name="Command Gateway"):
         self.display_gui(ctx)
 
     @commands.slash_command(
-            name="leavevc",
-            description="Prompt Clipped bot to leave your voice channel",
-            guild_ids=[os.getenv("DEV_GUILD_ID")])
+        name="leavevc",
+        description="Prompt Clipped bot to leave your voice channel",
+        guild_ids=[os.getenv("DEV_GUILD_ID")])
     async def cmd_leave_vc(self, ctx: discord.ApplicationContext):
         """ 
         Prompts Clipped bot to leave voice channel. This will stop any clipping 
@@ -40,7 +47,7 @@ class GatewayCog(Cog, name="Command Gateway"):
         """
         self.remove_gui(ctx)
         self.stop_capturing_voice(ctx)
-        self.leave_vc(ctx)
+        await self.leave_vc(ctx)
 
     ####### HELPER FUNCTIONS #######
 
@@ -48,17 +55,22 @@ class GatewayCog(Cog, name="Command Gateway"):
         voice = ctx.author.voice
 
         if voice is None:
-            await ctx.respond("You must be in a voice channel")
+            await ctx.respond(":warning: You must be in a voice channel")
             return None
-        
+
         try:
             voice_client: discord.VoiceClient = await voice.channel.connect()
         except Exception as e:
             print(e)
             return None
 
+        # Register voice session in DB
+        db.create_document(collection_name=self.CLIPPED_SESSIONS_COLLECTION,
+                           obj={"_id": ctx.guild_id,
+                                "channel_id": ctx.channel_id})
+
         return voice_client
-       
+
     def start_capturing_voice(self, voice: discord.VoiceClient) -> None:
         pass
 
@@ -71,8 +83,22 @@ class GatewayCog(Cog, name="Command Gateway"):
     def stop_capturing_voice(self, ctx: discord.ApplicationContext):
         pass
 
-    def leave_vc(self, ctx: discord.ApplicationContext):
-        pass
+    async def leave_vc(self, ctx: discord.ApplicationContext):
+        user_voice = ctx.author.voice
+        bot_voice = ctx.guild.voice_client
+
+        if user_voice is None:
+            await ctx.respond(":warning: You must be in a voice channel")
+            return
+        if bot_voice is None:
+            await ctx.respond(":warning: I'm not connected to a voice channel")
+            return
+
+        await bot_voice.disconnect()
+
+        # Remove voice session from DB
+        db.delete_document(collection_name=self.CLIPPED_SESSIONS_COLLECTION,
+                           id=ctx.guild_id)
 
     def fetch_clip(self):
         """Invokes search handler to retrieve clip based on natural language prompt."""
