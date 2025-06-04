01e88b0d15f0e383785af82ade36f0e8e11f8c65
Ryan Hou <rhou7873@gmail.com>
2025-06-04 13:11:19 -0400

    will be filtering opted out users at process time instead of stream time, not longer need to be fetching opt-in statuses in events handler...for now

--- modules/events_handler.py
+++ modules/events_handler.py
@@ -1,6 +1,6 @@
 # Clipped modules
 from bw_secrets import CLIPPED_SESSIONS_COLLECTION, BOT_USER_ID
-from modules.cmd_gateway import GatewayCog
+from modules.cmd_gateway import GatewayCog, DataStreamer
 import modules.database as db
 import ui
 
@@ -40,20 +40,12 @@ class EventsCog(Cog, name="Event Handler"):
         # Check DB to see if bot is in voice channel for this server,
         # and if so, retrieve the voice channel ID
         guild = member_updated.guild
-        query_result = db._read_document(collection_name=CLIPPED_SESSIONS_COLLECTION,
-                                         filter={"_id": guild.id},
-                                         projection={"_id": 0, "channel_id": 1})
-
-        # bot_channel_id=None would indicate bot isn't in a voice channel
-        bot_channel_id = query_result[0]["channel_id"] if len(
-            query_result) > 0 else None
 
         # `bot_joined_vc`: bot joined a VC with users in it
         # `bot_left_vc`: bot left a VC that it was in
         # `user_joined_bot_vc`: user joins VC with the bot in it
         bot_joined_vc = member_updated.id == BOT_USER_ID and after.channel is not None
         bot_left_vc = member_updated.id == BOT_USER_ID and after.channel is None
-        user_joined_bot_vc = after.channel is not None and after.channel.id == bot_channel_id
 
         if member_updated.id == BOT_USER_ID:  # when bot leaves/joins VC
             db.update_clipped_sessions(guild=guild,
@@ -62,18 +54,21 @@ class EventsCog(Cog, name="Event Handler"):
                                        bot_left_vc=bot_left_vc)
 
         if after.channel is not None:
-            await self._manage_voice_capture(member_updated=member_updated,
-                                             voice_channel=after.channel,
-                                             bot_joined_vc=bot_joined_vc,
-                                             user_joined_bot_vc=user_joined_bot_vc)
+            await self._notify_opt_in_options(voice_channel=after.channel)
 
-    async def _manage_voice_capture(self,
-                                    member_updated: discord.Member,
-                                    voice_channel: discord.VoiceChannel,
-                                    bot_joined_vc: bool,
-                                    user_joined_bot_vc: bool) -> None:
-        # If members in voice channel don't exist in database yet (i.e. first interaction
-        # with the bot), then send them DM w/ opt-in preference options
+        if bot_left_vc:
+            # Repetitive call to disconnect() (also called in
+            # cmd_gateway._leave_vc()), but ensures voice client
+            # is truly disconnected when user "right-click > disconnect"s
+            # instead of using /leavevc or clicking "Leave" button
+            DataStreamer.streams[guild.id].voice.disconnect(force=True)
+            del DataStreamer.streams[guild.id]
+
+    async def _notify_opt_in_options(self, voice_channel: discord.VoiceChannel) -> None:
+        """
+        If members in voice channel don't exist in database yet (i.e. first interaction
+        with the bot), then send them DM w/ opt-in preference options
+        """
         for member in voice_channel.members:
             if member.id == BOT_USER_ID:  # skip the bot itself
                 continue
@@ -90,22 +85,6 @@ class EventsCog(Cog, name="Event Handler"):
                               f"the '{member.guild.name}' server for any audio clips generated. "
                               "You may click either option below to opt-in or opt-out of "
                               "voice capture moving forward", view=view)
-
-        # In both of these scenarios, we want to fetch opt-in statuses
-        # before we start capturing voice data
-        new_opted_in_statuses = {}
-        if bot_joined_vc:
-            new_opted_in_statuses = db.get_opted_in_statuses(
-                members=voice_channel.members)
-        elif user_joined_bot_vc:
-            new_opted_in_statuses = db.get_opted_in_statuses(
-                members=[member_updated])
-
-        # TODO: now that we have opted-in statuses (of all users in VC that bot joined,
-        # or of the single member that just joined the VC), we want to use this information
-        # to manage whose voice data we're capturing
-        for member, opted_in in new_opted_in_statuses.items():
-            pass
 
 
 def setup(bot):
