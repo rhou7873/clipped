b8623f858838fb9c88f819c76999cbc1c0dd331a
Ryan Hou <rhou7873@gmail.com>
2025-07-21 13:38:01 -0400

    separated out steps of audio data processing pipeline into separate functions to prepare to reuse functionality for processing per-user wav audio

--- modules/data_processor.py
+++ modules/data_processor.py
@@ -38,7 +38,43 @@ class DataProcessor:
         outputted in WAV format.
         """
 
-        # Filter for "opted-in" users
+        # This outlines the outputs of each step within the
+        # audio data processing pipeline
+        filtered_data: List[Dict[int, sinks.AudioData]]
+        wav_chunks: List[Dict[int, io.BytesIO]]
+        standardized_wav_chunks: List[Dict[int, pydub.AudioSegment]]
+        overlayed_wav_chunks: List[pydub.AudioSegment]
+        clip: pydub.AudioSegment
+
+        # This is the actual series of steps in the pipeline
+        filtered_data = self._filter_opted_in_members()
+        wav_chunks = self._prepend_wav_headers(filtered_data)
+        standardized_wav_chunks = self._standardize_chunk_sizes(wav_chunks)
+        overlayed_wav_chunks = self._overlay_member_audios(
+            standardized_wav_chunks)
+        clip = self._concatenate_chunks(overlayed_wav_chunks)
+
+        # Return final clip as BytesIO object
+        clip_bytes_io = io.BytesIO()
+        clip.export(clip_bytes_io, format="wav")
+        clip_bytes_io.seek(0)
+        return clip_bytes_io
+
+    def process_audio_data_by_member(self):
+        """
+        Transforms raw AudioData chunks from Pycord into a list of 
+        synced, equal-length WAV clips for each member that spoke. 
+        This function is primarily used so per-member transcription
+        can be offloaded elsewhere.
+        """
+
+        filtered_data: List[Dict[int, sinks.AudioData]]
+        wav_chunks: List[Dict[int, io.BytesIO]]
+        standardized_wav_chunks: List[Dict[int, pydub.AudioSegment]]
+        clip_by_member: Dict[int, pydub.AudioSegment]
+
+    def _filter_opted_in_members(self):
+        """Filter for 'opted-in' users"""
         opted_in = [member.id
                     for member
                     in ClippedMember.get_opted_in_members(self.vc.channel.members)]
@@ -49,8 +85,13 @@ class DataProcessor:
                               if user_id in opted_in}
             filtered_data.append(filtered_chunk)
 
-        # Convert all users' audio data within each chunk to WAV format
-        # by prepending WAV headers
+        return filtered_data
+
+    def _prepend_wav_headers(self, filtered_data):
+        """
+        Convert all users' audio data within each chunk to 
+        WAV format by prepending WAV headers.
+        """
         wav_chunks: List[Dict[int, io.BytesIO]] = []
         for chunk in filtered_data:
             wav_chunk: Dict[int, io.BytesIO] = {}
@@ -67,7 +108,14 @@ class DataProcessor:
                 wav_chunk[user_id] = user_wav
             wav_chunks.append(wav_chunk)
 
-        # Standardize audio chunk sizes
+        return wav_chunks
+
+    def _standardize_chunk_sizes(self, wav_chunks):
+        """
+        Left-pad audio chunks with appropriate amount of silence so that
+        they are all synced and equal sized. This prepares the audio 
+        chunks for each member to be accurately overlayed.
+        """
         standardized_wav_chunks: List[Dict[int, pydub.AudioSegment]] = []
         for chunk in wav_chunks:
             standardized_wav_chunk = {}
@@ -102,7 +150,10 @@ class DataProcessor:
 
             standardized_wav_chunks.append(standardized_wav_chunk)
 
-        # Overlay all users' audio for each WAV audio chunk
+        return standardized_wav_chunks
+
+    def _overlay_member_audios(self, standardized_wav_chunks):
+        """Overlay all users' audio for each WAV audio chunk"""
         overlayed_wav_chunks: List[pydub.AudioSegment] = []
         for chunk in standardized_wav_chunks:
             # initialize base with a silent chunk
@@ -119,17 +170,49 @@ class DataProcessor:
 
             overlayed_wav_chunks.append(overlayed_wav_chunk)
 
-        # Concatenate WAV audio chunks into single clip
-        clip: pydub.AudioSegment = (pydub.AudioSegment
-                                    .silent(duration=0)
-                                    .set_channels(self.channels)
-                                    .set_sample_width(self.sampling_width)
-                                    .set_frame_rate(self.sampling_rate))
-        for chunk in overlayed_wav_chunks:
-            clip += chunk
+        return overlayed_wav_chunks
 
-        # Return final clip as BytesIO object
-        clip_bytes_io = io.BytesIO()
-        clip.export(clip_bytes_io, format="wav")
-        clip_bytes_io.seek(0)
-        return clip_bytes_io
+    def _concatenate_chunks(self, wav_chunks, overlay=True):
+        """
+        Concatenate WAV audio chunks into single clip. If the given
+        WAV chunks are an overlay of members, then this function will
+        output a pydub.AudioSegment. Otherwise, if it's not an overlay
+        (i.e. WAV chunks are still separated by user), then this function
+        will return a dictionary, mapping each user to their own 
+        concatenated pydub.AudioSegment.
+        """
+        clip = None
+        if overlay:
+            clip: pydub.AudioSegment = (pydub.AudioSegment
+                                        .silent(duration=0)
+                                        .set_channels(self.channels)
+                                        .set_sample_width(self.sampling_width)
+                                        .set_frame_rate(self.sampling_rate))
+
+            for chunk in wav_chunks:
+                if not isinstance(chunk, pydub.AudioSegment):
+                    raise Exception("Chunks within wav_chunks should be of type pydub.AudioSegment "
+                                    "if wav_chunks is an overlay of all members' audio. "
+                                    f"Instead got chunks of type '{type(chunk)}'")
+
+                clip += chunk
+        else:
+            clip: Dict[int, pydub.AudioSegment] = {}
+            
+            for chunk in wav_chunks:
+                if not isinstance(chunk, Dict):
+                    raise Exception("Chunks within wav_chunks should be of type Dict if wav_chunks "
+                                    "is not an overlay (i.e. audio data is separated by member). "
+                                    f"Instead got chunks of type '{type(chunk)}'")
+
+                for member, new_member_chunk in chunk.items():
+                    member_audio_segment = clip.get(member)
+                    if member_audio_segment is None:
+                        member_audio_segment = (pydub.AudioSegment
+                                                .silent(duration=0)
+                                                .set_channels(self.channels)
+                                                .set_sample_width(self.sampling_width)
+                                                .set_frame_rate(self.sampling_rate))
+                    clip[member] = member_audio_segment + new_member_chunk
+
+        return clip
