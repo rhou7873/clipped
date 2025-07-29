01a6e32abab33c3f72b23197b9165709a8dc9634
Ryan Hou <rhou7873@gmail.com>
2025-07-28 22:58:10 -0400

    removed the standardize chunk sizes function be no longer necessary if we just sync_start in the data streamer

--- modules/data_processor.py
+++ modules/data_processor.py
@@ -8,7 +8,7 @@ import discord.opus as op
 import discord.sinks as sinks
 
 # Other modules
-import io
+from io import BytesIO
 import pydub
 from typing import Dict, List
 import wave
@@ -32,7 +32,7 @@ class DataProcessor:
         self.sampling_width = op.Decoder.SAMPLE_SIZE // self.channels
         self.sampling_rate = op.Decoder.SAMPLING_RATE
 
-    def process_audio_data(self) -> io.BytesIO:
+    def process_audio_data(self) -> BytesIO:
         """
         Transforms raw AudioData chunks from Pycord into a "clip",
         outputted in WAV format.
@@ -41,7 +41,7 @@ class DataProcessor:
         # This outlines the outputs of each step within this
         # audio data processing pipeline
         filtered_data: List[Dict[discord.Member, sinks.AudioData]]
-        wav_chunks: List[Dict[discord.Member, io.BytesIO]]
+        wav_chunks: List[Dict[discord.Member, BytesIO]]
         standardized_wav_chunks: List[Dict[discord.Member, pydub.AudioSegment]]
         overlayed_wav_chunks: List[pydub.AudioSegment]
         clip: pydub.AudioSegment
@@ -49,13 +49,11 @@ class DataProcessor:
         # These actually carry out the series of steps in the pipeline
         filtered_data = self._filter_opted_in_members()
         wav_chunks = self._prepend_wav_headers(filtered_data)
-        standardized_wav_chunks = self._standardize_chunk_sizes(wav_chunks)
-        overlayed_wav_chunks = self._overlay_member_audios(
-            standardized_wav_chunks)
+        overlayed_wav_chunks = self._overlay_member_audios(wav_chunks)
         clip = self._concatenate_chunks(overlayed_wav_chunks, overlay=True)
 
         # Return final clip as BytesIO object
-        clip_bytes_io = io.BytesIO()
+        clip_bytes_io = BytesIO()
         clip.export(clip_bytes_io, format="wav")
         clip_bytes_io.seek(0)
 
@@ -72,8 +70,7 @@ class DataProcessor:
         # This outlines the outputs of each step within this
         # audio data processing pipeline
         filtered_data: List[Dict[discord.Member, sinks.AudioData]]
-        wav_chunks: List[Dict[discord.Member, io.BytesIO]]
-        standardized_wav_chunks: List[Dict[discord.Member, pydub.AudioSegment]]
+        wav_chunks: List[Dict[discord.Member, BytesIO]]
         clip_by_member: Dict[discord.Member, pydub.AudioSegment]
 
         # These actually carry out the series of the steps in the pipeline.
@@ -81,14 +78,12 @@ class DataProcessor:
         # the step of overlaying all members' audio.
         filtered_data = self._filter_opted_in_members()
         wav_chunks = self._prepend_wav_headers(filtered_data)
-        standardized_wav_chunks = self._standardize_chunk_sizes(wav_chunks)
-        clip_by_member = self._concatenate_chunks(
-            standardized_wav_chunks, overlay=False)
+        clip_by_member = self._concatenate_chunks(wav_chunks, overlay=False)
 
         # Convert users' AudioSegments to BytesIO for more flexibility downstream
-        clip_by_member_bytes: Dict[discord.Member, io.BytesIO] = {}
+        clip_by_member_bytes: Dict[discord.Member, BytesIO] = {}
         for user, audio in clip_by_member.items():
-            audio_bytes = io.BytesIO()
+            audio_bytes = BytesIO()
             audio.export(audio_bytes, format="wav")
             audio_bytes.seek(0)
             clip_by_member_bytes[user] = audio_bytes
@@ -114,11 +109,11 @@ class DataProcessor:
         Convert all users' audio data within each chunk to 
         WAV format by prepending WAV headers.
         """
-        wav_chunks: List[Dict[discord.Member, io.BytesIO]] = []
+        wav_chunks: List[Dict[discord.Member, BytesIO]] = []
         for chunk in filtered_data:
-            wav_chunk: Dict[discord.Member, io.BytesIO] = {}
+            wav_chunk: Dict[discord.Member, BytesIO] = {}
             for member, member_audio_data in chunk.items():
-                member_wav = io.BytesIO()
+                member_wav = BytesIO()
                 with wave.open(member_wav, "wb") as w:
                     # write WAV headers
                     w.setnchannels(self.channels)
@@ -132,53 +127,10 @@ class DataProcessor:
 
         return wav_chunks
 
-    def _standardize_chunk_sizes(self, wav_chunks):
-        """
-        Left-pad audio chunks with appropriate amount of silence so that
-        they are all synced and equal sized. This prepares the audio 
-        chunks for each member to be accurately overlayed.
-        """
-        standardized_wav_chunks: List[Dict[discord.Member, pydub.AudioSegment]]
-        standardized_wav_chunks = []
-        for chunk in wav_chunks:
-            standardized_wav_chunk = {}
-            for member, member_audio_data in chunk.items():
-                segment: pydub.AudioSegment = (pydub.AudioSegment
-                                               .from_file(member_audio_data,
-                                                          format="wav"))
-
-                # compute left padding needed
-                eps = 50
-                segment_size_ms = int(segment.duration_seconds * 1000)
-                target_size_ms = int(self.chunk_size * 1000)
-                padding_size_ms = target_size_ms - segment_size_ms
-
-                if padding_size_ms < -eps:
-                    raise Exception(
-                        "Length of audio chunk for a user shouldn't be longer "
-                        f"than chunk size, segment_size_ms={segment_size_ms}, "
-                        f"target_size_ms={target_size_ms}")
-
-                # generate silence
-                silence = (pydub.AudioSegment
-                           .silent(duration=max(padding_size_ms, 0))
-                           .set_channels(self.channels)
-                           .set_sample_width(self.sampling_width)
-                           .set_frame_rate(self.sampling_rate))
-
-                # prepend silence
-                padded_segment = silence + segment
-
-                standardized_wav_chunk[member] = padded_segment
-
-            standardized_wav_chunks.append(standardized_wav_chunk)
-
-        return standardized_wav_chunks
-
-    def _overlay_member_audios(self, standardized_wav_chunks):
+    def _overlay_member_audios(self, wav_chunks: List[Dict[discord.Member, BytesIO]]):
         """Overlay all users' audio for each WAV audio chunk"""
         overlayed_wav_chunks: List[pydub.AudioSegment] = []
-        for chunk in standardized_wav_chunks:
+        for chunk in wav_chunks:
             # initialize base with a silent chunk
             overlayed_wav_chunk = (pydub.AudioSegment
                                    .silent(duration=self.chunk_size * 1000)
@@ -188,14 +140,17 @@ class DataProcessor:
 
             # iteratively overlay all the users' voices
             for _, user_audio_data in chunk.items():
+                # Convert BytesIO to pydub.AudioSegment
+                user_audio_segment = pydub.AudioSegment.from_file(file=user_audio_data,
+                                                                  format="wav")
                 overlayed_wav_chunk = (overlayed_wav_chunk
-                                       .overlay(user_audio_data))
+                                       .overlay(user_audio_segment))
 
             overlayed_wav_chunks.append(overlayed_wav_chunk)
 
         return overlayed_wav_chunks
 
-    def _concatenate_chunks(self, wav_chunks, overlay=True):
+    def _concatenate_chunks(self, wav_chunks: List[Dict[discord.Member, BytesIO]], overlay=True):
         """
         Concatenate WAV audio chunks into single clip. If the given
         WAV chunks are an overlay of members, then this function will
@@ -236,6 +191,10 @@ class DataProcessor:
                                                 .set_channels(self.channels)
                                                 .set_sample_width(self.sampling_width)
                                                 .set_frame_rate(self.sampling_rate))
-                    clip[member] = member_audio_segment + new_member_chunk
+
+                    # Convert BytesIO to pydub.AudioSegment
+                    new_member_segment = pydub.AudioSegment.from_file(file=new_member_chunk,
+                                                                      format="wav")
+                    clip[member] = member_audio_segment + new_member_segment
 
         return clip
