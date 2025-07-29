7fadbf0377c408e6a4e9e10b2cccbb52ebcf73d6
Ryan Hou <rhou7873@gmail.com>
2025-07-28 22:16:54 -0400

    seconds to bytes and bytes to seconds utility functions in data streamer

--- modules/data_streamer.py
+++ modules/data_streamer.py
@@ -1,5 +1,6 @@
 # Pycord modules
 import discord
+import discord.opus as op
 from discord.sinks import Sink, PCMSink
 
 # Other modules
@@ -12,7 +13,8 @@ class DataStreamer:
                  voice: discord.VoiceClient,
                  clip_size: int = 30,
                  chunk_size: int = 1):
-        self.audio_data_buffer: Deque[dict] = Deque(maxlen=int(clip_size / chunk_size) + 1)
+        self.audio_data_buffer: Deque[dict] = Deque(
+            maxlen=int(clip_size / chunk_size) + 1)
         """Buffer of audio data, straight from Pycord"""
         self.voice = voice
         """Voice client we're streaming audio from"""
@@ -30,31 +32,18 @@ class DataStreamer:
         if self.is_streaming:
             return
 
-        sink_processed = asyncio.Event()
-
-        async def callback(sink: Sink):
-            if not self.is_streaming:  # stop() was called
-                return
-
-            current_chunk = sink.audio_data
-
-            # Add new chunk (oldest chunk will get flushed when
-            # buffer reaches its maxlen)
-            self.audio_data_buffer.append(current_chunk)
-
-            sink_processed.set()
-            sink_processed.clear()
+        async def noop_callback(sink: Sink):
+            pass
 
         async def stream_loop():
+            sink = PCMSink()
             self.is_streaming = True
+            self.voice.start_recording(sink, noop_callback)
+
             while True:
-                sink = PCMSink()
-
-                self.voice.start_recording(sink, callback)
                 await asyncio.sleep(self.chunk_size)
-                self.voice.stop_recording()
 
-                await sink_processed.wait()
+                # sink.audio_data.
 
         self.stream_loop_task = (asyncio
                                  .get_event_loop()
@@ -71,3 +60,24 @@ class DataStreamer:
             self.voice.stop_recording()
 
         self.stream_loop_task.cancel()
+
+    @staticmethod
+    def bytes_to_seconds(num_bytes: int) -> float:
+        """Utility function to calculate seconds of audio, given length of bytes."""
+        sampling_rate = op.Decoder.SAMPLING_RATE
+        sample_size = op.Decoder.SAMPLE_SIZE
+
+        duration = num_bytes / (sampling_rate * sample_size)
+
+        return duration
+
+    @staticmethod
+    def seconds_to_bytes(duration: float) -> int:
+        """Utility function to calculate number of bytes, given seconds of audio."""
+        sampling_rate = op.Decoder.SAMPLING_RATE
+        sample_size = op.Decoder.SAMPLE_SIZE
+
+        num_bytes = int(duration * sampling_rate * sample_size)
+        print(f"\n\n{num_bytes}\n\n")
+
+        return num_bytes
